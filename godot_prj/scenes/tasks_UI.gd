7100dac0efdb8f4dcc1e93a55d6d2a3ba57d9ddd
Radu Bolovan <rbolovan@amberstudio.com>
2021-07-08 09:13:14 +0300

    Fix tasks list

--- godot_prj/scenes/tasks_UI.gd
+++ godot_prj/scenes/tasks_UI.gd
@@ -7,8 +7,8 @@ func _ready():
 	$close_btn.connect("pressed", self, "on_close")
 
 func initialize(tasks):
-	for idx in range($list/content.get_child_count(), 0, -1):
-		$list/content.remove_child($list/content.get_child(idx))
+	for child in $list/content.get_children():
+		child.queue_free()
 
 	for idx in range(0, tasks.size()):
 		var task_ui = load("res://scenes/task_UI.tscn").instance()
